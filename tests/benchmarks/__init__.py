ca0393ab9c146a0ce945f1bdd3ff8e76902e8862
saschabuehrle <sascha.buehrle@gmail.com>
2025-11-20 20:45:04 +0100

    Update benchmark __init__ to export profiler

--- tests/benchmarks/__init__.py
+++ tests/benchmarks/__init__.py
@@ -1,14 +1,19 @@
 """Benchmark framework components."""
 
-from .base import Benchmark, BenchmarkResult
+from .base import Benchmark, BenchmarkResult, BenchmarkSummary
 from .metrics import CostMetrics, LatencyMetrics, QualityMetrics
+from .profiler import CascadeProfile, CascadeProfiler, ComponentProfile
 from .reporter import BenchmarkReporter
 
 __all__ = [
     "Benchmark",
     "BenchmarkResult",
+    "BenchmarkSummary",
     "CostMetrics",
     "LatencyMetrics",
     "QualityMetrics",
     "BenchmarkReporter",
+    "CascadeProfile",
+    "CascadeProfiler",
+    "ComponentProfile",
 ]
